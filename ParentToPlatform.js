ee283fcad3c95326e2722d8041e67b2463c8b313
Tanner Fisher <lmha8@hotmail.com>
2016-02-13 01:34:16 -0500

    ParentToPlatform Script Added
    After attaching the "MovingPlatform" script to the platform you would
    like to move, create an empty game object as a child to the platform.
    Place a box collider component on the child and set it to isTrigger.
    Raise the box collider slightly above the platforms collider so the
    collider on the player will enter it. The script will make the Player a
    child of the platform while the Player is within the trigger collider.

---
+++ ParentToPlatform.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function OnTriggerEnter(other : Collider){ // when collider enters trigger collider
+                 
+    if(other.tag == "Player"){ // if the other colliders tag is Player
+        other.transform.parent = transform.parent; // make the parent of the Player collider the same as the parent of the trigger collider
+     
+    }
+}
+     
+function OnTriggerExit(other : Collider) { // when collider exits trigger collider
+
+    if(other.tag == "Player") {
+        other.transform.parent = null; // reset the Player coliders parent to null
+                 
+    }
+}
