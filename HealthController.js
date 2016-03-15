7920139676d743ea32805a968b3da88477780bb7
Tanner Fisher <lmha8@hotmail.com>
2016-03-15 02:02:56 -0400

    Added scripts for Lives UI and for controlling the Start Menu
    The HealthController is for changing the sprite image on the UI element
    display the number of hearts (lives) the player has left.  The
    MenuController is for the start menu and level selection.

---
+++ HealthController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var lives3 : Sprite; // Player has full health
+var lives2 : Sprite; // Player has 2 lives left
+var lives1 : Sprite; // Player has 1 life left
+var lives0 : Sprite; // Player has 0 lives left
+
+private var currentLives : UnityEngine.UI.Image;
+
+static var LIVES = 3; // A global variable to hold the value of lives that the player possesses
+     
+function Start () {
+	currentLives = gameObject.GetComponent.<UnityEngine.UI.Image>(); // Set the Image component of the gameObject to be currentLives
+	Debug.Log("Lives left = " +LIVES); // How many lives are left
+}
+
+function Update () {
+	if (LIVES == 3) { // If the Player has 3 lives
+		currentLives.sprite = lives3; // Set the Image sprite to show 3 active hearts
+	}
+	else if (LIVES == 2) {
+		currentLives.sprite = lives2;
+	}
+	else if (LIVES == 1) {
+		currentLives.sprite = lives1;
+	}
+	else {
+		currentLives.sprite = lives0;
+	}
+}
---
+++ MenuController.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+// Place this script on the Menu Controller Object
+// Drag appropriate Buttons on to their matching variables
+// use the onClick on the buttons by dragging the Menu Controller Object onto the field to acces the functions within this script
+
+import UnityEngine.SceneManagement; // Make use of the SceneManager
+
+// Level Buttons //////////////////////////////
+public var level1 : UnityEngine.UI.Button;
+public var level2 : UnityEngine.UI.Button;
+public var level3 : UnityEngine.UI.Button;
+
+public function startLevel1 () {
+		SceneManager.LoadScene("The Feared Forest");
+
+}
+
+public function startLevel2 () {
+		SceneManager.LoadScene("The Lost Limbo");
+
+}
+
+public function startLevel3 () {
+		SceneManager.LoadScene("The Jungle Jaws");
+
+}
+
+public function exitGame () {
+	Application.Quit();
+}
