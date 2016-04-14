051366476fc02b8888afd6aa768f9cb91d701ac8
Tanner Fisher <lmha8@hotmail.com>
2016-04-14 04:06:22 -0400

    Pushing more scripts

--- Treant.js
+++ Treant.js
@@ -10,6 +10,7 @@ var stun : boolean;
 var pRb : Rigidbody;
 public var hitPoints : int;
 private var reqHits : int;
+public var MoveSpeed : int;
 
 public var particle : GameObject;
 public var clip : AudioClip;
@@ -23,6 +24,10 @@ function Start() {
 }
 
 function FixedUpdate(){
+  GetComponent.<Rigidbody>().AddForce(new Vector3(0, -10*GetComponent.<Rigidbody>().mass, 0));
+  if(transform.position.y <= -10){
+    Destroy(gameObject);
+  }
   stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 
   dist = (Player.transform.position - transform.position);
@@ -32,7 +37,14 @@ function FixedUpdate(){
       anim.SetBool('cast spell', true);
       StartCoroutine(WaitForAggro(1.2));
     } else if(aggro) {
-      anim.SetBool('walk', true);
+      anim.SetBool('cast spell', false);
+      anim.SetBool('run', true);
+      var lookPos = Player.transform.position - transform.position;
+      lookPos.y = 0;
+      var rotation = Quaternion.LookRotation(lookPos);
+      transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 4);
+      //transform.LookAt(Player.transform.position);
+      transform.position = Vector2.MoveTowards(transform.position, new Vector2(Player.transform.position.x, transform.position.y), MoveSpeed * Time.deltaTime);
     } else {
       anim.SetBool('cast spell', false);
     }
@@ -53,6 +65,7 @@ function WaitForAggro(time : float){
 
 function OnTriggerEnter(col:Collider){
   if(col.tag == 'Player'){
+    Player.GetComponent.<PlayerDamage>().nextHitAllowed = Time.time + 1.0;
     pRb.velocity = new Vector3(0,10,0);
     hitPoints--;
     if(hitPoints > 0){
@@ -73,12 +86,13 @@ function OnTriggerEnter(col:Collider){
 
 public function StunTimer(){
   yield new WaitForSeconds(1.5f);
-  var effect : GameObject = Instantiate(particle, Vector3((gameObject.transform.position.x+2),(gameObject.transform.position.y+1.5),gameObject.transform.position.z), transform.rotation);
+  var effect : GameObject = Instantiate(particle, Vector3((gameObject.transform.Find("RigHeadGizmo").position.x),(gameObject.transform.Find("RigHeadGizmo").position.y+1.5),gameObject.transform.Find("RigHeadGizmo").position.z), transform.rotation);
   effect.transform.localScale = new Vector3 (3,1,3);
   yield new WaitForSeconds(4f);
   effect.AddComponent.<DestroyParticle>();
+  anim.SetBool('die', false);
+  yield new WaitForSeconds(1f);
   stun = false;
   hitPoints = reqHits;
-  anim.SetBool('die', false);
   anim.SetBool('run', true);
 }
